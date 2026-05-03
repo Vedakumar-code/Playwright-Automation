5b9eb1382c078e27ea13ad091eaa82c0202c86e1
Vedakumar <vedarjun73@gmail.com>
2026-05-03 16:30:50 +0530

    updated checkout page

--- WebPages/checkoutPage.js
+++ WebPages/checkoutPage.js
@@ -4,12 +4,15 @@ import { expect } from "@playwright/test";
 export class checkoutPage extends basePage {
     constructor(page) {
         super(page);
+        this.reviewYourOrderLocator = page.locator("//h2[normalize-space()='Review Your Order']");
         this.placeOrderBtnLocator = page.getByRole('link', { name: 'Place Order' });
+
 
     }
 
     async clickPlaceOrder() {
         //await expect(this.placeOrderBtnLocator).toBeVisible();
+        await expect(this.reviewYourOrderLocator).toBeVisible();
         await this.placeOrderBtnLocator.scrollIntoViewIfNeeded();
         await this.click(this.placeOrderBtnLocator);
     }
