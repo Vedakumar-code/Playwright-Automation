d26c959c6399d3786073608b220bc6e1ff2a0dd3
Vedakumar <vedarjun73@gmail.com>
2026-05-03 17:09:15 +0530

    updated checkout page

--- WebPages/checkoutPage.js
+++ WebPages/checkoutPage.js
@@ -5,18 +5,20 @@ export class checkoutPage extends basePage {
     constructor(page) {
         super(page);
         this.placeOrderBtnLocator = page.locator("#cart_items .btn.btn-default.check_out");
-        this.modalLocator = page.locator('#checkoutModal');
-
+        //this.modalLocator = page.locator('#checkoutModal');
+        this.checkoutModal = page.locator('#checkoutModal');
 
     }
 
     async clickPlaceOrder() {
 
-        if (await this.modalLocator.isVisible()) {
-            await this.page.locator('a[href="/login"]').click();
-        }
+        // if (await this.modalLocator.isVisible()) {
+        //     await this.page.locator('a[href="/login"]').click();
+        // }
 
-        await this.placeOrderBtnLocator.scrollIntoViewIfNeeded();
+        await this.checkoutModal.waitFor({ state: 'hidden' });
+
+        // await this.placeOrderBtnLocator.scrollIntoViewIfNeeded();
         await this.click(this.placeOrderBtnLocator);
     }
 
